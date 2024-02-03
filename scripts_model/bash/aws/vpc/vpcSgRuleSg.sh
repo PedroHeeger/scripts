2f344b75c4368ce5aa5e7e4e0b323ccecef1c0d5
Pedro <pedroheeger19@gmail.com>
2024-02-03 09:59:48 -0300

    Upload scripts_model
    Criando modelos AWS (vpc_sg_rule e vpc_sg_rule_sg)

--- scripts_model/bash/aws/vpc/vpcSgRuleSg.sh
+++ scripts_model/bash/aws/vpc/vpcSgRuleSg.sh
@@ -2,7 +2,7 @@
 
 echo "***********************************************"
 echo "SERVIÇO: AWS EC2-VPC"
-echo "SECURITY GROUP RULE CREATION"
+echo "SECURITY GROUP RULE SG CREATION"
 
 echo "-----//-----//-----//-----//-----//-----//-----"
 echo "Definindo variáveis"
@@ -14,10 +14,11 @@ sgRuleDescription="sgRuleDescriptionTest1"
 fromPort="22"
 toPort="22"
 protocol="tcp"
-cidrIpv4="0.0.0.0/0"
+referencedGroupName="bia-db-teste"
+# referencedGroupName="sgVincTest1"
 
 echo "-----//-----//-----//-----//-----//-----//-----"
-read -p "Deseja executar o código? (y/n) " resposta
+read -p "Deseja executar o código? (s/n) " resposta
 if [ "$(echo "$resposta" | tr '[:upper:]' '[:lower:]')" == "y" ]; then
     echo "-----//-----//-----//-----//-----//-----//-----"
     echo "Verificando se a VPC é a padrão ou não"
@@ -31,44 +32,44 @@ if [ "$(echo "$resposta" | tr '[:upper:]' '[:lower:]')" == "y" ]; then
 
     echo "-----//-----//-----//-----//-----//-----//-----"
     echo "Verificando se existe a VPC $vpcName"
-    if [ $(aws ec2 describe-vpcs --filters "Name=$condition,Values=$vpcNameControl" --query "Vpcs[].VpcId" --output text | wc -l) -gt 1 ]; then
+    if [ $(aws ec2 describe-vpcs --filters "Name=$condition,Values=$vpcNameControl" --query "Vpcs[].VpcId" --output text | wc -w) -gt 1 ]; then
         echo "-----//-----//-----//-----//-----//-----//-----"
         echo "Extraindo o Id da VPC $vpcName"
         vpcId=$(aws ec2 describe-vpcs --filters "Name=$condition,Values=$vpcNameControl" --query "Vpcs[].VpcId" --output text)
 
         echo "-----//-----//-----//-----//-----//-----//-----"
         echo "Verificando se existe o security group $sgName na VPC $vpcName"
-        if [ $(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpcId" "Name=group-name,Values=$sgName" --query "SecurityGroups[].GroupName" --output text | wc -l) -gt 1 ]; then
+        if [ $(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpcId" "Name=group-name,Values=$sgName" --query "SecurityGroups[].GroupName" --output text | wc -w) -gt 1 ]; then
             echo "-----//-----//-----//-----//-----//-----//-----"
             echo "Extraindo o Id do security group $sgName da VPC $vpcName"
             sgId=$(aws ec2 describe-security-groups --query "SecurityGroups[?GroupName=='$sgName'].GroupId" --output text)
-           
+
             echo "-----//-----//-----//-----//-----//-----//-----"
-            echo "Verificando se existe uma regra liberando a porta $fromPort no security group $sgName"
-            existRule=$(aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && CidrIpv4=='$cidrIpv4']" --output text)
-            if [ -n "$existRule" ]; then
+            echo "Extraindo o Id do security group vinculado $referencedGroupName"
+            referencedGroupId=$(aws ec2 describe-security-groups --query "SecurityGroups[?GroupName=='$referencedGroupName'].GroupId" --output text)
+
+            echo "-----//-----//-----//-----//-----//-----//-----"
+            echo "Verificando se existe uma regra de entrada liberando a porta $fromPort do security group $sgName da VPC $vpcName"
+            existRule=$(aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && ReferencedGroupInfo.GroupId=='$referencedGroupId']")
+            if [ $(echo "$existRule" | grep -c "GroupId") -gt 1 ]; then
                 echo "-----//-----//-----//-----//-----//-----//-----"
                 echo "Já existe a regra de entrada liberando a porta $fromPort do security group $sgName da VPC $vpcName"
-                aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && CidrIpv4=='$cidrIpv4'].GroupId" --output text
+                aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && ReferencedGroupInfo.GroupId=='$referencedGroupId'].GroupId" --output text
             else
                 echo "-----//-----//-----//-----//-----//-----//-----"
                 echo "Listando o Id de todas as regras de entrada do security group $sgName da VPC $vpcName"
                 aws ec2 describe-security-group-rules --filters "Name=group-id,Values=$sgId" --query "SecurityGroupRules[?!IsEgress].SecurityGroupRuleId" --output text
 
-                # echo "-----//-----//-----//-----//-----//-----//-----"
-                # echo "Adicionando uma regra de entrada ao security group $sgName da VPC $vpcName para liberação da porta $fromPort"
-                # aws ec2 authorize-security-group-ingress --group-id $sgId --protocol $protocol --port $fromPort --cidr $cidrIpv4 --description $sgRuleDescription --no-cli-pager
-            
                 echo "-----//-----//-----//-----//-----//-----//-----"
                 echo "Adicionando uma regra de entrada ao security group $sgName da VPC $vpcName para liberação da porta $fromPort"
-                aws ec2 authorize-security-group-ingress --group-id $sgId --ip-permissions "IpProtocol=$protocol,FromPort=$fromPort,ToPort=$toPort,IpRanges=[{CidrIp=$cidrIpv4,Description='$sgRuleDescription'}]" --no-cli-pager
+                aws ec2 authorize-security-group-ingress --group-id $sgId --ip-permissions "IpProtocol=$protocol,FromPort=$fromPort,ToPort=$toPort,UserIdGroupPairs=[{GroupId=$referencedGroupId,Description='$sgRuleDescription'}]" --no-cli-pager
 
                 echo "-----//-----//-----//-----//-----//-----//-----"
                 echo "Listando o Id da regra de entrada do security group $sgName da VPC $vpcName que libera a porta $fromPort"
-                aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && CidrIpv4=='$cidrIpv4'].GroupId" --output text
+                aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && ReferencedGroupInfo.GroupId=='$referencedGroupId'].GroupId" --output text
             fi
         else
-            echo "Não existe o security group $sgName na VPC $vpcName"
+            echo "Não existe o security group $sgName"
         fi
     else
         echo "Não existe a VPC $vpcName"
@@ -82,9 +83,11 @@ fi
 
 #!/bin/bash
 
+#!/bin/bash
+
 echo "***********************************************"
 echo "SERVIÇO: AWS EC2-VPC"
-echo "SECURITY GROUP RULE EXCLUSION"
+echo "SECURITY GROUP RULE SG EXCLUSION"
 
 echo "-----//-----//-----//-----//-----//-----//-----"
 echo "Definindo variáveis"
@@ -93,9 +96,9 @@ vpcName="default"
 # sgName="sgTest1"
 # vpcName="vpcTest1"
 protocol="tcp"
-fromPort="22"
-toPort="22"
-cidrIpv4="0.0.0.0/0"
+port="22"
+referencedGroupName="bia-db-teste"
+# referencedGroupName="sgVincTest1"
 
 echo "-----//-----//-----//-----//-----//-----//-----"
 read -p "Deseja executar o código? (y/n) " resposta
@@ -112,43 +115,43 @@ if [ "$(echo "$resposta" | tr '[:upper:]' '[:lower:]')" == "y" ]; then
 
     echo "-----//-----//-----//-----//-----//-----//-----"
     echo "Verificando se existe a VPC $vpcName"
-    if [ $(aws ec2 describe-vpcs --filters "Name=$condition,Values=$vpcNameControl" --query "Vpcs[].VpcId" --output text | wc -l) -gt 1 ]; then
+    if [ $(aws ec2 describe-vpcs --filters "Name=$condition,Values=$vpcNameControl" --query "Vpcs[].VpcId" --output text | wc -w) -gt 1 ]; then
         echo "-----//-----//-----//-----//-----//-----//-----"
         echo "Extraindo o Id da VPC $vpcName"
         vpcId=$(aws ec2 describe-vpcs --filters "Name=$condition,Values=$vpcNameControl" --query "Vpcs[].VpcId" --output text)
 
         echo "-----//-----//-----//-----//-----//-----//-----"
         echo "Verificando se existe o security group $sgName na VPC $vpcName"
-        if [ $(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpcId" "Name=group-name,Values=$sgName" --query "SecurityGroups[].GroupName" --output text | wc -l) -gt 1 ]; then
+        if [ $(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpcId" "Name=group-name,Values=$sgName" --query "SecurityGroups[].GroupName" --output text | wc -w) -gt 1 ]; then
             echo "-----//-----//-----//-----//-----//-----//-----"
             echo "Extraindo o Id do security group $sgName"
             sgId=$(aws ec2 describe-security-groups --query "SecurityGroups[?GroupName=='$sgName'].GroupId" --output text)
-   
+
             echo "-----//-----//-----//-----//-----//-----//-----"
-            echo "Verificando se existe uma regra liberando a porta $fromPort no security group $sgName"
-            existRule=$(aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$fromPort' && to_string(ToPort)=='$toPort' && CidrIpv4=='$cidrIpv4']" --output text)
-            if [ -n "$existRule" ]; then
+            echo "Extraindo o Id do security group vinculado $referencedGroupName"
+            referencedGroupId=$(aws ec2 describe-security-groups --query "SecurityGroups[?GroupName=='$referencedGroupName'].GroupId" --output text)
+
+            echo "-----//-----//-----//-----//-----//-----//-----"
+            echo "Verificando se existe uma regra de entrada liberando a porta $port do security group $sgName da VPC $vpcName"
+            existRule=$(aws ec2 describe-security-group-rules --query "SecurityGroupRules[?GroupId=='$sgId' && !IsEgress && IpProtocol=='$protocol' && to_string(FromPort)=='$port' && to_string(ToPort)=='$port' && ReferencedGroupInfo.GroupId=='$referencedGroupId']" --output text)
+            if [ $(echo "$existRule" | wc -w) -gt 1 ]; then
                 echo "-----//-----//-----//-----//-----//-----//-----"
                 echo "Listando o Id de todas as regras de entrada do security group $sgName"
                 aws ec2 describe-security-group-rules --filters "Name=group-id,Values=$sgId" --query "SecurityGroupRules[?!IsEgress].SecurityGroupRuleId" --output text
-    
-                # echo "-----//-----//-----//-----//-----//-----//-----"
-                # echo "Removendo a regra de entrada do security group $sgName para liberação da porta $fromPort"
-                # aws ec2 revoke-security-group-ingress --group-id $sgId --protocol $protocol --port $fromPort --cidr $cidrIpv4
 
                 echo "-----//-----//-----//-----//-----//-----//-----"
-                echo "Removendo a regra de entrada do security group $sgName para liberação da porta $fromPort"
-                aws ec2 revoke-security-group-ingress --group-id $sgId --ip-permissions "IpProtocol=$protocol,FromPort=$fromPort,ToPort=$toPort,IpRanges=[{CidrIp=$cidrIpv4}]" --no-cli-pager
-    
+                echo "Removendo a regra de entrada do security group $sgName para liberação da porta $port"
+                aws ec2 revoke-security-group-ingress --group-id $sgId --ip-permissions "IpProtocol=$protocol,FromPort=$port,ToPort=$port,UserIdGroupPairs=[{GroupId=$referencedGroupId}]" --no-cli-pager
+
                 echo "-----//-----//-----//-----//-----//-----//-----"
                 echo "Listando o Id de todas as regras de entrada do security group $sgName"
                 aws ec2 describe-security-group-rules --filters "Name=group-id,Values=$sgId" --query "SecurityGroupRules[?!IsEgress].SecurityGroupRuleId" --output text
 
             else
-                echo "Não existe a regra de entrada liberando a porta $fromPort no security group $sgName"
+                echo "Não existe a regra de entrada liberando a porta $port no security group $sgName"
             fi
         else
-            echo "Não existe o security group $sgName na VPC $vpcName"
+            echo "Não existe o security group $sgName"
         fi
     else
         echo "Não existe a VPC $vpcName"
--- scripts_model/python/aws/vpc/vpcSgRuleSg.py
+++ scripts_model/python/aws/vpc/vpcSgRuleSg.py
@@ -4,7 +4,7 @@ import boto3
 
 print("***********************************************")
 print("SERVIÇO: AWS EC2-VPC")
-print("SECURITY GROUP RULE CREATION")
+print("SECURITY GROUP RULE SG CREATION")
 
 print("-----//-----//-----//-----//-----//-----//-----")
 print("Definindo variáveis")
@@ -13,12 +13,12 @@ vpc_name = "default"
 # sg_name = "sgTest1"
 # vpc_name = "vpcTest1"
 sg_rule_description = "sgRuleDescriptionTest1"
-from_port = "21"
-to_port = "21"
+from_port = 22
+to_port = 22
 protocol = "tcp"
-cidr_ipv4 = "0.0.0.0/0"
+referenced_group_name = "bia-db-teste"
+# referenced_group_name = "sgVincTest1"
 
-print("-----//-----//-----//-----//-----//-----//-----")
 resposta = input("Deseja executar o código? (y/n) ")
 if resposta.lower() == 'y':
     print("-----//-----//-----//-----//-----//-----//-----")
@@ -36,35 +36,40 @@ if resposta.lower() == 'y':
 
     print("-----//-----//-----//-----//-----//-----//-----")
     print(f"Verificando se existe a VPC {vpc_name}")
-    vpcs = ec2_client.describe_vpcs(Filters=[{'Name': condition, 'Values': [vpc_name_control]}])['Vpcs']
-
-    if len(vpcs) > 0:
+    response = ec2_client.describe_vpcs(Filters=[{'Name': condition, 'Values': [vpc_name_control]}])
+    if len(response['Vpcs']) > 0:
         print("-----//-----//-----//-----//-----//-----//-----")
         print(f"Extraindo o Id da VPC {vpc_name}")
-        vpc_id = vpcs[0]['VpcId']
+        vpc_id = response['Vpcs'][0]['VpcId']
 
         print("-----//-----//-----//-----//-----//-----//-----")
         print(f"Verificando se existe o security group {sg_name} na VPC {vpc_name}")
-        sg_groups = ec2_client.describe_security_groups(Filters=[
+        response = ec2_client.describe_security_groups(Filters=[
             {'Name': 'vpc-id', 'Values': [vpc_id]},
             {'Name': 'group-name', 'Values': [sg_name]}
-        ])['SecurityGroups']
-
-        if len(sg_groups) > 0:
+        ])
+        if len(response['SecurityGroups']) > 0:
             print("-----//-----//-----//-----//-----//-----//-----")
             print(f"Extraindo o Id do security group {sg_name} da VPC {vpc_name}")
-            sg_id = sg_groups[0]['GroupId']
+            sg_id = response['SecurityGroups'][0]['GroupId']
 
             print("-----//-----//-----//-----//-----//-----//-----")
-            print(f"Verificando se existe uma regra de entrada liberando a porta {from_port} do security group {sg_name} da VPC {vpc_name}")     
+            print(f"Extraindo o Id do security group vinculado {referenced_group_name}")
+            responseSg2 = ec2_client.describe_security_groups(Filters=[
+                {'Name': 'group-name', 'Values': [referenced_group_name]}
+            ])
+            referenced_group_id = responseSg2['SecurityGroups'][0]['GroupId']
+
+            print("-----//-----//-----//-----//-----//-----//-----")
+            print(f"Verificando se existe uma regra de entrada liberando a porta {from_port} do security group {sg_name} da VPC {vpc_name}")
             exist_rule = []
-            for sg_group in sg_groups:
+            for sg_group in response['SecurityGroups']:
                 for rule in sg_group.get('IpPermissions', []):
                     if (
                         rule.get('IpProtocol') == protocol and
                         rule.get('FromPort') == int(from_port) and
                         rule.get('ToPort') == int(to_port) and
-                        any(ip_range['CidrIp'] == cidr_ipv4 for ip_range in rule.get('IpRanges', []))
+                        any(user_pair['GroupId'] == referenced_group_id for user_pair in rule.get('UserIdGroupPairs', []))
                     ):
                         exist_rule.append(sg_group['GroupId'])
 
@@ -85,7 +90,19 @@ if resposta.lower() == 'y':
                 print(f"Adicionando uma regra de entrada ao security group {sg_name} da VPC {vpc_name} para liberação da porta {from_port}")
                 ec2_client.authorize_security_group_ingress(
                     GroupId=sg_id,
-                    IpPermissions=[{'IpProtocol': protocol, 'FromPort': int(from_port), 'ToPort': int(to_port), 'IpRanges': [{'CidrIp': cidr_ipv4}]}]
+                    IpPermissions=[
+                        {
+                            'IpProtocol': protocol,
+                            'FromPort': from_port,
+                            'ToPort': to_port,
+                            'UserIdGroupPairs': [
+                                {
+                                    'GroupId': referenced_group_id,
+                                    'Description': sg_rule_description
+                                }
+                            ]
+                        }
+                    ]
                 )
 
                 print("-----//-----//-----//-----//-----//-----//-----")
@@ -102,12 +119,12 @@ if resposta.lower() == 'y':
                             rule.get('IpProtocol') == protocol and
                             rule.get('FromPort') == int(from_port) and
                             rule.get('ToPort') == int(to_port) and
-                            any(ip_range['CidrIp'] == cidr_ipv4 for ip_range in rule.get('IpRanges', []))
+                            any(user_pair['GroupId'] == referenced_group_id for user_pair in rule.get('UserIdGroupPairs', []))
                         ):
                             exist_rule.append(sg_group['GroupId'])
                 print(exist_rule)
         else:
-            print(f"Não existe o security group {sg_name} na VPC {vpc_name}")
+            print(f"Não existe o security group {sg_name}")
     else:
         print(f"Não existe a VPC {vpc_name}")
 else:
@@ -122,7 +139,7 @@ import boto3
 
 print("***********************************************")
 print("SERVIÇO: AWS EC2-VPC")
-print("SECURITY GROUP RULE EXCLUSION")
+print("SECURITY GROUP RULE SG EXCLUSION")
 
 print("-----//-----//-----//-----//-----//-----//-----")
 print("Definindo variáveis")
@@ -131,11 +148,11 @@ vpc_name = "default"
 # sg_name = "sgTest1"
 # vpc_name = "vpcTest1"
 protocol = "tcp"
-from_port = "21"
-to_port = "21"
-cidr_ipv4 = "0.0.0.0/0"
+from_port = 22
+to_port = 22
+referenced_group_name = "bia-db-teste"
+# referenced_group_name = "sgVincTest1"
 
-print("-----//-----//-----//-----//-----//-----//-----")
 resposta = input("Deseja executar o código? (y/n) ")
 if resposta.lower() == 'y':
     print("-----//-----//-----//-----//-----//-----//-----")
@@ -153,32 +170,42 @@ if resposta.lower() == 'y':
 
     print("-----//-----//-----//-----//-----//-----//-----")
     print(f"Verificando se existe a VPC {vpc_name}")
-    vpcs = ec2_client.describe_vpcs(Filters=[{'Name': condition, 'Values': [vpc_name_control]}])['Vpcs']
-
-    if len(vpcs) > 0:
+    response = ec2_client.describe_vpcs(Filters=[{'Name': condition, 'Values': [vpc_name_control]}])
+    if len(response['Vpcs']) > 0:
         print("-----//-----//-----//-----//-----//-----//-----")
         print(f"Extraindo o Id da VPC {vpc_name}")
-        vpc_id = vpcs[0]['VpcId']
+        vpc_id = response['Vpcs'][0]['VpcId']
 
         print("-----//-----//-----//-----//-----//-----//-----")
         print(f"Verificando se existe o security group {sg_name} na VPC {vpc_name}")
-        sg_groups = ec2_client.describe_security_groups(Filters=[
+        response = ec2_client.describe_security_groups(Filters=[
             {'Name': 'vpc-id', 'Values': [vpc_id]},
             {'Name': 'group-name', 'Values': [sg_name]}
-        ])['SecurityGroups']
-
-        if len(sg_groups) > 0:
+        ])
+        if len(response['SecurityGroups']) > 0:
             print("-----//-----//-----//-----//-----//-----//-----")
             print(f"Extraindo o Id do security group {sg_name}")
-            sg_id = sg_groups[0]['GroupId']
+            sg_id = response['SecurityGroups'][0]['GroupId']
 
             print("-----//-----//-----//-----//-----//-----//-----")
-            print(f"Verificando se existe uma regra liberando a porta {from_port} no security group {sg_name}")
-            exist_rule = [rule for rule in sg_groups[0].get('IpPermissions', []) if
-                rule.get('IpProtocol') == protocol and
-                rule.get('FromPort') == int(from_port) and
-                rule.get('ToPort') == int(to_port) and
-                any(ip_range['CidrIp'] == cidr_ipv4 for ip_range in rule.get('IpRanges', []))]
+            print(f"Extraindo o Id do security group vinculado {referenced_group_name}")
+            responseSg2 = ec2_client.describe_security_groups(Filters=[
+                {'Name': 'group-name', 'Values': [referenced_group_name]}
+            ])
+            referenced_group_id = responseSg2['SecurityGroups'][0]['GroupId']
+
+            print("-----//-----//-----//-----//-----//-----//-----")
+            print(f"Verificando se existe uma regra de entrada liberando a porta {from_port} do security group {sg_name} da VPC {vpc_name}")
+            exist_rule = []
+            for sg_group in response['SecurityGroups']:
+                for rule in sg_group.get('IpPermissions', []):
+                    if (
+                        rule.get('IpProtocol') == protocol and
+                        rule.get('FromPort') == int(from_port) and
+                        rule.get('ToPort') == int(to_port) and
+                        any(user_pair['GroupId'] == referenced_group_id for user_pair in rule.get('UserIdGroupPairs', []))
+                    ):
+                        exist_rule.append(sg_group['GroupId'])
 
             if len(exist_rule) > 0:
                 print("-----//-----//-----//-----//-----//-----//-----")
@@ -193,7 +220,18 @@ if resposta.lower() == 'y':
                 print(f"Removendo a regra de entrada do security group {sg_name} para liberação da porta {from_port}")
                 ec2_client.revoke_security_group_ingress(
                     GroupId=sg_id,
-                    IpPermissions=[{'IpProtocol': protocol, 'FromPort': int(from_port), 'ToPort': int(to_port), 'IpRanges': [{'CidrIp': cidr_ipv4}]}]
+                    IpPermissions=[
+                        {
+                            'IpProtocol': protocol,
+                            'FromPort': from_port,
+                            'ToPort': to_port,
+                            'UserIdGroupPairs': [
+                                {
+                                    'GroupId':  referenced_group_id
+                                }
+                            ]
+                        }
+                    ]
                 )
 
                 print("-----//-----//-----//-----//-----//-----//-----")
@@ -203,7 +241,6 @@ if resposta.lower() == 'y':
                 )['SecurityGroupRules']
                 ingress_rules = [rule['SecurityGroupRuleId'] for rule in all_rules if not rule['IsEgress']]
                 print(ingress_rules)
-
             else:
                 print(f"Não existe a regra de entrada liberando a porta {from_port} no security group {sg_name}")
         else:
